380ad71ea0dd9e31b111d2562731c68c735e5c24
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2025-03-03 17:25:38 -0300

    Inclui o método findByNome no CategoriaRepository.

--- src/main/java/br/com/tarefas_api/repository/CategoriaRepository.java
+++ src/main/java/br/com/tarefas_api/repository/CategoriaRepository.java
@@ -3,9 +3,13 @@ package br.com.tarefas_api.repository;
 import br.com.tarefas_api.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Repositório responsável pelas operações CRUD na entidade Categoria.
  */
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findByNome(String nome);
 
 }

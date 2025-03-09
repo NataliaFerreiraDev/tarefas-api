276ee0b3d39cd56ea2590d0504639d87527abb14
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2025-03-09 19:55:54 -0300

    Correcao das migrations.

--- src/main/resources/db/migration/V20250224175100__create_item_table.sql
+++ src/main/resources/db/migration/V20250224175100__create_item_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE item (
     descricao VARCHAR(255) NOT NULL,
     concluido BOOLEAN NOT NULL DEFAULT FALSE,
     data_limite TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     categoria_id UUID NOT NULL,
     CONSTRAINT fk_item_categoria FOREIGN KEY (categoria_id) REFERENCES categoria(id) ON DELETE CASCADE
